e84cd3cdf0c3e490e5b21bc7c2d56fe94044d5fd
KaptainAhoibrause <132769719+KaptainAhoibrause@users.noreply.github.com>
2024-08-09 19:11:38 +0200

    added loading game scores

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
-init_health = 200
-init_energy = 200
-init_strength_factor = 1.0
-init_distance_walked = 0
-init_food = 15
-init_mobs_found = 0
+import json
+init_health = None
+init_energy = None
+init_strength_factor = None
+init_distance_walked = None
+init_food = None
+init_mobs_found = None
 
 
 class Game:
     def __init__(self):
         pass
 
-    def start(self):
-        pass
+    @staticmethod
+    def start():
+        global init_health, init_energy, init_strength_factor, init_distance_walked, init_food, init_mobs_found
+        with open("score.json", "r") as f:
+            game_score = json.load(f)
+        if len(list(game_score)) == 0:
+            init_health = 200
+            init_energy = 200
+            init_strength_factor = 1.0
+            init_distance_walked = 0
+            init_food = 15
+            init_mobs_found = 0
+        else:
+            for x in game_score.keys():
+                exec("{fx} = {fvalue}".format(fx=x, fvalue=game_score[x]))
 
     def restart(self):
         pass
@@ -41,6 +55,9 @@ class Player:
         pass
 
     def mob_found(self):
+        pass
+
+    def item_found(self):
         pass
 
 

32315d38b31507f52522bb165cea17663e92b8d2
KaptainAhoibrause <132769719+KaptainAhoibrause@users.noreply.github.com>
2024-08-09 18:15:29 +0200

    refactored structure of Player class

--- src/main.py
+++ src/main.py
@@ -4,24 +4,32 @@ init_distance_to_treasure = 100
 init_food = 15
 init_mobs_found = 0
 
-class activities():
+
+class Player:
     def __init__(self):
         self.health = init_health
         self.energy = init_energy
         self.distance_to_treasure = init_distance_to_treasure
         self.food = init_food
         self.mobs_found = init_mobs_found
+
     def walking(self):
         pass
+
     def eating(self):
         pass
+
     def animal_found(self):
         pass
+
     def mob_found(self):
         pass
+
     def fighting(self):
         pass
+
     def treasure(self):
         pass
+
     def restart(self):
         pass

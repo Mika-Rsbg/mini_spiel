db6cd9e68e5b33a59b65b94e78c3d05f70d3b2e0
Mika-Rsbg <ro-mika@web.de>
2024-08-09 15:06:38 +0200

    added base functions

---
+++ src/main.py
@@ -0,0 +1,21 @@
+class activities():
+    def __init__(self):
+        self.health = 200
+        self.energy = 200
+        self.distance_treasure = 100
+        self.nutrition = 15
+        self.mobs_found = 0
+    def walking(self):
+        pass
+    def eating(self):
+        pass
+    def animal_found(self):
+        pass
+    def mob_found(self):
+        pass
+    def fighting(self):
+        pass
+    def treasure(self):
+        pass
+    def restart(self):
+        pass
